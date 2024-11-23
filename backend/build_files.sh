17ca090fa1ac2c89c81dfe8aabd7a5368427a384
Mahfuz Mia <sayhello.mahfuz@gmail.com>
2024-11-24 04:48:21 +0600

    setup deployment on vercel

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,2 +1,14 @@
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+#!/bin/bash
+
+set -e  # Exit on error
+
+echo "BUILD START"
+
+# Use the specific version of Python
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
