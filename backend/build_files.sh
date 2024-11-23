3f61adde206f4fa4424afdcf314ffc2b90e775f3
Mahfuz Mia <sayhello.mahfuz@gmail.com>
2024-11-24 03:07:49 +0600

    setup deployment on vercel

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -5,8 +5,8 @@ set -e  # Exit on error
 echo "BUILD START"
 
 # Install dependencies
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 # Collect static files
 export DJANGO_SETTINGS_MODULE=GradBond.settings  # Adjust to your settings module
