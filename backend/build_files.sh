42f1226b5827d417be041d537545a4a1327c52e1
mahfuz1703 <sayhello.mahfuz@gmail.com>
2025-05-22 01:27:46 +0600

    Railway deployment setup

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -3,4 +3,4 @@ python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
-daphne GradBond.asgi:application
+daphne -b 0.0.0.0 -p $PORT GradBond.asgi:application
